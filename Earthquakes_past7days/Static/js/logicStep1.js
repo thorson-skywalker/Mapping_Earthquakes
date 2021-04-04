3bf425aff5b03d8977b147f94cfd6809ce8ab933
Luke Terrell <luke.terrell43@gmail.com>
2021-04-04 09:23:20 -0500

    added map that could display all earthquakes within the past week

---
+++ Earthquakes_past7days/Static/js/logicStep1.js
@@ -0,0 +1,56 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+});
+
+// Create base layer that holds both maps
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with a center and zoom level. [40.7, -94.5]
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
+});
+
+// , {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer)
+//         // add popup for each neighborhood
+//         layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2")
+//     }
+// }
+
+// // Create a style for the lines
+// let myStyle = {
+//     color: "blue",
+//     weight: 1,
+//     fillColor: "yellow"
+// }
